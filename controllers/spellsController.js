9f17f80fe99a20caffc0371559416d91d2a9befa
thongster <106802301+thongster@users.noreply.github.com>
2026-01-13 14:14:17 +0700

    wrote query function for add spell, update spell

---
+++ controllers/spellsController.js
@@ -0,0 +1,3 @@
+if (updated === 0) {
+  // spell not found OR bad category
+}
--- db/queries.js
+++ db/queries.js
@@ -12,21 +12,55 @@ async function getAllElements() {
 
 async function getSpellById(id) {
   const { rows } = await pool.query('SELECT * FROM spells WHERE id = $1', [id]);
-  return rows;
+  return rows[0];
 }
 
 async function getSpellByElement(element) {
   const { rows } = await pool.query(
-    'SELECT * FROM spells JOIN categories ON spells.category_id = categories.id WHERE category.name = $1',
+    `SELECT * FROM spells 
+    JOIN categories ON spells.category_id = categories.id 
+    WHERE categories.name = $1`,
     [element]
+  );
+  return rows;
+}
+
+async function addSpell(
+  name,
+  description,
+  mana,
+  cooldown,
+  damage,
+  range,
+  element
+) {
+  await pool.query(
+    `INSERT INTO spells (name, description, mana, cooldown, damage, range, category_id) 
+    VALUES ($1, $2, $3, $4, $5, $6, (SELECT id FROM categories WHERE name = $7))`,
+    [name, description, mana, cooldown, damage, range, element]
   );
 }
 
-async function addSpell() {
-  console.log(pool);
-}
+async function updateSpell(
+  name,
+  description,
+  mana,
+  cooldown,
+  damage,
+  range,
+  element,
+  id
+) {
+  const { rowCount } = await pool.query(
+    `UPDATE spells
+        SET name = $1, description = $2, mana = $3, cooldown = $4, damage = $5, range = $6,
+        category_id = (SELECT id FROM categories WHERE name = $7)
+        WHERE id = $8`,
+    [name, description, mana, cooldown, damage, range, element, id]
+  );
 
-async function updateSpell(id) {}
+  return rowCount;
+}
 
 async function deleteSpell(id) {}
 
